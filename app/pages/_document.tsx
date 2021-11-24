3d7dcf0ab759db63272c5c4ca8ea7173a89a9e64
Daniel Burger <public@danielburger.online>
2021-11-24 11:51:36 +0100

    Add Three.js and correct Favicons etc.

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,10 +1,16 @@
-import { Document, Html, DocumentHead, Main, BlitzScript /*DocumentContext*/ } from "blitz"
+import { Document, Html, DocumentHead, Main, BlitzScript } from "blitz"
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <DocumentHead />
+        <title>Daniel Burger — Neurotech Software Engineer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Full-stack software engineer with several years of interdisciplinary work experience in a technologically diverse background dedicated to clean code and lean architectures. Currently, I'm developing brain-machine interface software at IDUN Technologies and studying web development with a focus on spatial computing at Middlesex University."
+        />
         <body>
           <Main />
           <BlitzScript />
