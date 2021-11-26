7cd16be71e65d324e79375af652ee6e98bac63b4
Daniel Burger <public@danielburger.online>
2021-11-26 10:00:00 +0100

    More refactoring

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,73 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope, faCalendar } from "@fortawesome/free-solid-svg-icons"
+import HeroDecorations from "./components/HeroDecorations"
 
 export default function Hero() {
   return (
     <div className="relative flex items-center justify-center h-screen overflow-hidden bg-gray-900 min-h-small">
-      <div
-        className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
-        aria-hidden="true"
-      >
-        <div className="relative h-full mx-auto max-w-7xl">
-          <svg
-            className="absolute transform right-full translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-800"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-          <svg
-            className="absolute transform left-full -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-            width={404}
-            height={784}
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-800"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
-          </svg>
-        </div>
-      </div>
-
+      <HeroDecorations />
       <div className="relative pt-6 pb-16 sm:pb-24">
         <main className="px-6 mx-auto mt-16 max-w-7xl sm:mt-24">
           <div className="text-center">
