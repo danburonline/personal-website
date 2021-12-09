19af224b74d27cabcd3ae44f058944b3c4ca5e09
Daniel Burger <public@danielburger.online>
2021-12-09 09:20:33 +0100

    Add one more TODO

--- app/components/Daniel/components/DanielModel.tsx
+++ app/components/Daniel/components/DanielModel.tsx
@@ -60,6 +60,7 @@ export default function DanielModel({
   // TODO Add a more compressed version of the model
   // TODO Add more interesting post-processing effects and filters
   // TODO Animate the eyes of the 3D model to blink sometimes
+  // TODO Fix the max rotation of the model
 
   const { nodes } = useGLTF("./models/daniel-transformed.glb") as unknown as DanielModelGLTFResult
   return (
