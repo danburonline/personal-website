34c2dc51632fd515f5dbf502e8f7b7fbb9cc45c7
Daniel Burger <public@danielburger.online>
2021-12-12 17:37:52 +0100

    Adjust base for Skill section
    Prepare for adding the 3D models

--- app/components/Skills/Skills.tsx
+++ app/components/Skills/Skills.tsx
@@ -1,34 +1,16 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { CheckIcon } from "@heroicons/react/outline"
-
-const hobbyFeatures = [
-  "Pariatur quod similique",
-  "Sapiente libero doloribus",
-  "Vel ipsa esse repudiandae",
-]
-const scaleFeatures = [
-  "Pariatur quod similique",
-  "Sapiente libero doloribus",
-  "Vel ipsa esse repudiandae",
-]
-const growthFeatures = [
-  "Quia rem est sed impedit magnam",
-  "Dolorem vero ratione voluptates",
-  "Qui sed ab doloribus voluptatem dolore",
-  "Laborum commodi molestiae id et fugiat",
-  "Nam ut ipsa nesciunt culpa modi dolor",
-]
+import { neuroscienceList, artificialIntelligenceList, virtualRealityList } from "./data/skillLists"
 
 export default function Skills() {
   return (
     <div className="bg-gray-900">
       <div className="px-4 pt-12 sm:px-6 lg:px-8 lg:pt-20">
         <div className="text-center">
-          <h2 className="text-lg font-semibold leading-6 tracking-wider text-gray-300 uppercase">
-            Pricing
+          <h2 className="text-lg font-semibold leading-6 tracking-wider uppercase text-primary">
+            Skills
           </h2>
           <p className="mt-2 text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">
-            The right price for you, whoever you are
+            Combining different disciplines
           </p>
           <p className="max-w-4xl mx-auto mt-3 text-xl text-gray-300 sm:mt-5 sm:text-2xl">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos
@@ -42,29 +24,13 @@ export default function Skills() {
           <div className="absolute inset-0 bg-gray-900 h-5/6 lg:h-2/3" />
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative lg:grid lg:grid-cols-7">
-              <div className="max-w-md mx-auto lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
+              <div className="max-w-lg mx-auto lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
                 <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-l-lg">
                   <div className="flex flex-col flex-1">
-                    <div className="px-6 py-10 bg-white">
-                      <div>
-                        <h3
-                          className="text-2xl font-medium text-center text-gray-900"
-                          id="tier-hobby"
-                        >
-                          Hobby
-                        </h3>
-                        <div className="flex items-center justify-center mt-4">
-                          <span className="flex items-start px-3 text-6xl tracking-tight text-gray-900">
-                            <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                            <span className="font-extrabold">79</span>
-                          </span>
-                          <span className="text-xl font-medium text-gray-500">/month</span>
-                        </div>
-                      </div>
-                    </div>
+                    <div className="bg-white h-72"></div>
                     <div className="flex flex-col justify-between flex-1 p-6 border-t-2 border-gray-100 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
                       <ul role="list" className="space-y-4">
-                        {hobbyFeatures.map((feature) => (
+                        {neuroscienceList.map((feature) => (
                           <li key={feature} className="flex items-start">
                             <div className="flex-shrink-0">
                               <CheckIcon
@@ -76,17 +42,6 @@ export default function Skills() {
                           </li>
                         ))}
                       </ul>
-                      <div className="mt-8">
-                        <div className="rounded-lg shadow-md">
-                          <a
-                            href="#"
-                            className="block w-full px-6 py-3 text-base font-medium text-center text-indigo-600 bg-white border border-transparent rounded-lg hover:bg-gray-50"
-                            aria-describedby="tier-hobby"
-                          >
-                            Start your trial
-                          </a>
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -94,36 +49,13 @@ export default function Skills() {
               <div className="max-w-lg mx-auto mt-10 lg:mt-0 lg:max-w-none lg:mx-0 lg:col-start-3 lg:col-end-6 lg:row-start-1 lg:row-end-4">
                 <div className="relative z-10 rounded-lg shadow-xl">
                   <div
-                    className="absolute inset-0 border-2 border-indigo-600 rounded-lg pointer-events-none"
+                    className="absolute inset-0 rounded-lg pointer-events-none"
                     aria-hidden="true"
                   />
-                  <div className="absolute inset-x-0 top-0 transform translate-y-px">
-                    <div className="flex justify-center transform -translate-y-1/2">
-                      <span className="inline-flex px-4 py-1 text-sm font-semibold tracking-wider text-white uppercase bg-indigo-600 rounded-full">
-                        Most popular
-                      </span>
-                    </div>
-                  </div>
-                  <div className="px-6 pt-12 pb-10 bg-white rounded-t-lg">
-                    <div>
-                      <h3
-                        className="text-3xl font-semibold text-center text-gray-900 sm:-mx-6"
-                        id="tier-growth"
-                      >
-                        Growth
-                      </h3>
-                      <div className="flex items-center justify-center mt-4">
-                        <span className="flex items-start px-3 text-6xl tracking-tight text-gray-900 sm:text-6xl">
-                          <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                          <span className="font-extrabold">149</span>
-                        </span>
-                        <span className="text-2xl font-medium text-gray-500">/month</span>
-                      </div>
-                    </div>
-                  </div>
+                  <div className="bg-white rounded-t-lg lg:h-96 h-72"></div>
                   <div className="px-6 pt-10 pb-8 border-t-2 border-gray-100 rounded-b-lg bg-gray-50 sm:px-10 sm:py-10">
                     <ul role="list" className="space-y-4">
-                      {growthFeatures.map((feature) => (
+                      {virtualRealityList.map((feature) => (
                         <li key={feature} className="flex items-start">
                           <div className="flex-shrink-0">
                             <CheckIcon
@@ -135,43 +67,16 @@ export default function Skills() {
                         </li>
                       ))}
                     </ul>
-                    <div className="mt-10">
-                      <div className="rounded-lg shadow-md">
-                        <a
-                          href="#"
-                          className="block w-full px-6 py-4 text-xl font-medium leading-6 text-center text-white bg-indigo-600 border border-transparent rounded-lg hover:bg-indigo-700"
-                          aria-describedby="tier-growth"
-                        >
-                          Start your trial
-                        </a>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
-              <div className="max-w-md mx-auto mt-10 lg:m-0 lg:max-w-none lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3">
+              <div className="max-w-lg mx-auto mt-10 lg:m-0 lg:max-w-none lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3">
                 <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-r-lg">
                   <div className="flex flex-col flex-1">
-                    <div className="px-6 py-10 bg-white">
-                      <div>
-                        <h3
-                          className="text-2xl font-medium text-center text-gray-900"
-                          id="tier-scale"
-                        >
-                          Scale
-                        </h3>
-                        <div className="flex items-center justify-center mt-4">
-                          <span className="flex items-start px-3 text-6xl tracking-tight text-gray-900">
-                            <span className="mt-2 mr-2 text-4xl font-medium">$</span>
-                            <span className="font-extrabold">349</span>
-                          </span>
-                          <span className="text-xl font-medium text-gray-500">/month</span>
-                        </div>
-                      </div>
-                    </div>
+                    <div className="bg-white h-72"></div>
                     <div className="flex flex-col justify-between flex-1 p-6 border-t-2 border-gray-100 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
                       <ul role="list" className="space-y-4">
-                        {scaleFeatures.map((feature) => (
+                        {artificialIntelligenceList.map((feature) => (
                           <li key={feature} className="flex items-start">
                             <div className="flex-shrink-0">
                               <CheckIcon
@@ -183,17 +88,6 @@ export default function Skills() {
                           </li>
                         ))}
                       </ul>
-                      <div className="mt-8">
-                        <div className="rounded-lg shadow-md">
-                          <a
-                            href="#"
-                            className="block w-full px-6 py-3 text-base font-medium text-center text-indigo-600 bg-white border border-transparent rounded-lg hover:bg-gray-50"
-                            aria-describedby="tier-scale"
-                          >
-                            Start your trial
-                          </a>
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
---
+++ app/components/Skills/data/skillLists.ts
@@ -0,0 +1,17 @@
+const neuroscienceList = [
+  "Pariatur quod similique",
+  "Sapiente libero doloribus",
+  "Vel ipsa esse repudiandae",
+]
+const artificialIntelligenceList = [
+  "Pariatur quod similique",
+  "Sapiente libero doloribus",
+  "Vel ipsa esse repudiandae",
+]
+const virtualRealityList = [
+  "Quia rem est sed impedit magnam",
+  "Dolorem vero ratione voluptates",
+  "Qui sed ab doloribus voluptatem dolore",
+]
+
+export { neuroscienceList, artificialIntelligenceList, virtualRealityList }
