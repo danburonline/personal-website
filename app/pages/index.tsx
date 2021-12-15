1e1ce00d39e19dea0b5fd52fa2209c1755abfd9b
Daniel Burger <public@danielburger.online>
2021-12-15 16:22:39 +0100

    Lazy load below the fold components

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,26 +1,32 @@
-import "@fortawesome/fontawesome-svg-core/styles.css"
 import { BlitzPage } from "blitz"
 import { dynamic } from "blitz"
 import { config } from "@fortawesome/fontawesome-svg-core"
+import { Waypoint } from "react-waypoint"
+import { useState } from "react"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import Hero from "../components/Hero"
 import About from "../components/About"
 import Navigation from "app/components/Navigation"
-import Skills from "app/components/Skills"
 
 const LazyDaniel = dynamic(() => import("../components/Daniel"), {
   ssr: false,
 })
-
-// TODO Add initial animations for the Hero components
+const LazySkills = dynamic(() => import("app/components/Skills"), {
+  ssr: false,
+})
 
 const Home: BlitzPage = () => {
+  const [loadBelowTheFold, setLoadBelowTheFold] = useState(false)
+
   return (
     <>
       <LazyDaniel />
       <Navigation />
       <Hero />
       <About />
-      <Skills />
+      <Waypoint onEnter={() => setLoadBelowTheFold(true)}>
+        <div>{loadBelowTheFold && <LazySkills />}</div>
+      </Waypoint>
     </>
   )
 }
