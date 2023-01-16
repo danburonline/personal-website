066cdd92d2b9da2659266e34db2c938ba05d9767
Daniel Burger <public@danielburger.online>
2023-01-16 19:46:28 +0100

    Do dynamic routes work on CloudFlare?

--- src/pages/api/posts/[id].ts
+++ src/pages/api/posts/[id].ts
@@ -1,11 +1,11 @@
 import type { NextRequest } from "next/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export default async function handler(_req: NextRequest): Promise<Response> {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
+  return new Response(JSON.stringify({ message: "Hello World" }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
