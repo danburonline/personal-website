3fb14b3b1801beed1ca31312eb31a0b43aa517e8
Daniel Burger <public@danielburger.online>
2023-01-16 17:59:13 +0100

    Rename edge runtime in API endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,10 @@
 import type { NextRequest } from "next/server";
 
 export const config = {
-  runtime: "experimental-edge",
+  runtime: "edge",
 };
 
-export default async function handler(req: NextRequest): Promise<Response> {
+export default async function handler(_req: NextRequest): Promise<Response> {
   return new Response(JSON.stringify({ name: "John Doe" }), {
     status: 200,
     headers: {
