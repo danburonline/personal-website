927804d56e1e09f2b6b162b757392eae028f9c53
Daniel Burger <public@danielburger.online>
2023-04-04 17:51:28 +0200

    Social media adjustments again...
    Maybe I finally made a decision on which
    platforms I want to settle and build a
    personal brand on.

--- app/components/Navigation/data/socialMediaChannels.ts
+++ app/components/Navigation/data/socialMediaChannels.ts
@@ -6,9 +6,10 @@ import {
   faLinkedin,
   faTwitter,
   faYoutube,
-  faFigma,
+  faInstagram,
+  faBehance,
 } from "@fortawesome/free-brands-svg-icons"
-import { faCode } from "@fortawesome/free-solid-svg-icons"
+import { faDatabase } from "@fortawesome/free-solid-svg-icons"
 
 export type socialMediaChannelType = {
   name: string
@@ -20,6 +21,13 @@ export type socialMediaChannelType = {
 
 const socialMediaChannels: socialMediaChannelType[] = [
   {
+    name: "ResearchGate",
+    description: "Research papers and essays of all my public research projects",
+    href: "https://www.researchgate.net/profile/Daniel-Burger-10",
+    icon: faResearchgate,
+    bgColor: "#00ccbb",
+  },
+  {
     name: "GitHub",
     description: "Source code, wikis and issues of all my public programming projects",
     href: "https://github.com/danburonline",
@@ -27,11 +35,11 @@ const socialMediaChannels: socialMediaChannelType[] = [
     bgColor: "#24292e",
   },
   {
-    name: "ResearchGate",
-    description: "Research papers and essays of all my public research projects",
-    href: "https://www.researchgate.net/profile/Daniel-Burger-10",
-    icon: faResearchgate,
-    bgColor: "#00ccbb",
+    name: "Hugging Face",
+    description: "Models, datasets and other resources of all my public ML projects",
+    href: "https://huggingface.co/danburonline",
+    icon: faDatabase,
+    bgColor: "#F8D44E",
   },
   {
     name: "Medium",
@@ -48,18 +56,11 @@ const socialMediaChannels: socialMediaChannelType[] = [
     bgColor: "#fb000a",
   },
   {
-    name: "Replit",
-    description: "Repls, demos and code snippets of all my public programming projects",
-    href: "https://replit.com/@danburonline",
-    icon: faCode,
-    bgColor: "#F26207",
-  },
-  {
-    name: "Figma",
-    description: "Visual designs, prototypes and mockups of all my public design projects",
-    href: "https://www.figma.com/@danburonline",
-    icon: faFigma,
-    bgColor: "#212121",
+    name: "Behance",
+    description: "Designs, 3D models and other creative stuff of all my public creative work",
+    href: "https://www.behance.net/danburonline",
+    icon: faBehance,
+    bgColor: "#2356F6",
   },
   {
     name: "LinkedIn",
@@ -74,6 +75,13 @@ const socialMediaChannels: socialMediaChannelType[] = [
     href: "https://twitter.com/danburonline",
     icon: faTwitter,
     bgColor: "#08a0e9",
+  },
+  {
+    name: "Instagram",
+    description: "Pictures, videos and stories about my life, travels and other random stuff",
+    href: "https://instagram.com/danburonline",
+    icon: faInstagram,
+    bgColor: "#e4405f",
   },
 ]
 
