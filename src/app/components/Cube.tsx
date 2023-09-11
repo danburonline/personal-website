599e4450fe8695fe3eea37b49a5d3c9be2625333
Daniel Burger <public@danielburger.online>
2023-09-11 17:35:49 +0200

    Add R3F Drei and remove example frame animation

--- src/app/components/Cube.tsx
+++ src/app/components/Cube.tsx
@@ -1,29 +1,8 @@
-import { useFrame } from '@react-three/fiber'
-import { useRef, useState } from 'react'
-
 export default function Box(props: any) {
-  const meshRef = useRef<any>()
-
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  useFrame((_state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta
-    }
-  })
-
   return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
+    <mesh {...props}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? '#FF40FF' : '#1A001A'} />
+      <meshStandardMaterial color="#FF40FF" />
     </mesh>
   )
 }
--- src/app/components/Scene.tsx
+++ src/app/components/Scene.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import Box from './Cube'
 
 export default function Scene() {
   return (
     <Canvas>
+      <OrbitControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box />
     </Canvas>
   )
 }
